12ebe1342c81f1566d2b1957c5e67c5eefae9b0e
Muhammad Sattarov <sattarovmuhammad98@gmail.com>
2024-06-25 14:44:51 +0500

    Initial Commit

---
+++ Namoz vaqti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Namoz vaqti
+//
+//  Created by user on 25/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
